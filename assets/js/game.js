939c7f8f8d3b17a098454cd20780796d4ad15f3f
NatBat92 <natalie.neale1992@outlook.com>
2022-10-15 21:19:34 +0000

    add form and table elements to create table for card contents and add styling to buttons

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,27 @@
-let data_header = ["Name", "Power", "Magic", "Terror", "Wisdom"];
+// God  and mythical creature arrays
 
-// God arrays
-let hades = ["Austria", 8.77, 83858, 104.6, 5.6, 10.6, 82];
-let cronus = ["Belgium", 11.35, 30510, 372.1, 5.4, 7.8, 81];
-let persephone = ["Bulgaria", 7.1, 110912, 64, -7.3, 5.8, 75];
-let aphrodite = ["Croatia", 4.15, 56594, 73.4, -11.8, 8.6, 77];
-let apollo = ["Cyprus", 0.85, 9250, 92.4, 11, 8.2, 80];
-let ares = ["Czech Republic", 10.58, 78866, 134.1, 2.9, 2.3, 79];
-let artemis = ["Denmark", 5.75, 43094, 133.4, 5.6, 4.3, 81];
-let zeus = ["Estonia", 1.32, 45226, 29.1, 2.7, 6.5, 77];
-let athena = ["Finland", 5.5, 337030, 16.3, 1.8, 8.2, 81];
-let poseidon = ["France", 66.99, 643548, 104.1, 3.5, 10.6, 83];
-let hera = ["Germany", 82.52, 357021, 231.1, 4, 5.5, 81];
-let hermes = ["Greece", 10.77, 131957, 81.6, -2.7, 21.7, 82];
-let hestia = ["hungary", 9.8, 93030, 105.3, -2, 3.8, 76];
-let iris = ["Ireland", 4.78, 70280, 68.1, 11.2, 5.1, 82];
-let atlas = ["Italy", 60.59, 301320, 201.1, -1.7, 11.3, 83];
-let rhea = ["Latvia", 1.95, 64589, 30.2, -8.1, 10.3, 74];
-let basilisk = ["Lithuania", 2.85, 65200, 43.7, -13.8, 7.4, 75];
-let harpy = ["Luxembourg", 0.59, 2586, 228.4, 19, 6.7, 82];
-let pheonix = ["Malta", 0.46, 316, 1456.6, 32.9, 5.1, 82];
-let siren = ["Netherlands", 17.08, 41526, 411.3, 2.1, 4.8, 82];
+let gods = [ 
+    new mythologicalCard ("hades", 100, 65, 90, 25),
+    new mythologicalCard ("cronus", 100, 65, 90, 25),
+    new mythologicalCard ("persephone", 100, 65, 90, 25),
+    new mythologicalCard ("aphrodite", 100, 65, 90, 25),
+    new mythologicalCard ("apollo", 100, 65, 90, 25),
+    new mythologicalCard ("ares", 100, 65, 90, 25),
+    new mythologicalCard ("artemis", 100, 65, 90, 25),
+    new mythologicalCard ("zeus", 100, 65, 90, 25),
+    new mythologicalCard ("athena", 100, 65, 90, 25),
+    new mythologicalCard ("poseidon", 100, 65, 90, 25),
+    new mythologicalCard ("hera", 100, 65, 90, 25),
+    new mythologicalCard ("hermes", 100, 65, 90, 25),
+    new mythologicalCard ("hestia", 100, 65, 90, 25),
+    new mythologicalCard ("iris", 100, 65, 90, 25),
+    new mythologicalCard ("atlas", 100, 65, 90, 25),
+    new mythologicalCard ("rhea", 100, 65, 90, 25),
+    new mythologicalCard ("basilisk", 100, 65, 90, 25),
+    new mythologicalCard ("harpy", 100, 65, 90, 25),
+    new mythologicalCard ("pheonix", 100, 65, 90, 25),
+    new mythologicalCard ("siren", 100, 65, 90, 25),
+];
 
 let allCards = [hades, cronus, persephone, aphrodite, apollo, ares, artemis, zeus, athena, poseidon, hera, hermes, hestia, iris, atlas, rhea, basilisk, harpy, pheonix, siren]
 
