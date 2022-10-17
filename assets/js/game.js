5ff68b055ab1fba537c18ade5bc91537a8eff772
NatBat92 <natalie.neale1992@outlook.com>
2022-10-17 20:11:49 +0000

    change gods array so that it has an index number and can be called correctly

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,66 +1,60 @@
 // God  and mythical creature arrays
 
-let gods = [ 
-    new mythologicalCard ("hades", 100, 65, 90, 25),
-    new mythologicalCard ("cronus", 50, 20, 30, 65),
-    new mythologicalCard ("persephone", 0, 40, 0, 20),
-    new mythologicalCard ("aphrodite", 70, 70, 0, 85),
-    new mythologicalCard ("apollo", 70, 25, 90, 60),
-    new mythologicalCard ("ares", 100, 0, 100, 50),
-    new mythologicalCard ("artemis", 25, 0, 50, 70),
-    new mythologicalCard ("zeus", 100, 90, 90, 70),
-    new mythologicalCard ("athena", 85, 50, 70, 100),
-    new mythologicalCard ("poseidon", 100, 100, 100, 70),
-    new mythologicalCard ("hera", 100, 20, 0, 75),
-    new mythologicalCard ("hermes", 20, 60, 0, 90),
-    new mythologicalCard ("hestia", 30, 70, 15, 80),
-    new mythologicalCard ("iris", 0, 0, 0, 50),
-    new mythologicalCard ("atlas", 100, 40, 0, 100),
-    new mythologicalCard ("rhea", 80, 90, 25, 65),
-    new mythologicalCard ("basilisk", 100, 0, 90, 0),
-    new mythologicalCard ("harpy", 70, 50, 90, 30),
-    new mythologicalCard ("pheonix", 70, 90, 25, 70),
-    new mythologicalCard ("siren", 100, 80, 90, 40),
-];
+let gods = [];
 
-var content = "";
-var result = "";
-var endGame = false;
-var noGo = false;
-var noValue = -1;
-let playerCards;
-let computerCards;
-var player, computer, theDeck, playerOneCard, playerTwoCard, ending, computerStack, playerStack, mainStack, g, i, j, cardNum;
+gods[0] = ["hades", 100, 65, 90, 25];
+gods[1] = ["cronus", 50, 20, 30, 65];
 
 
-function draw() {
-    if (endGame) {
-        ending = "cards";
-        playerCards = player.getSize()+ending;
-        computerCards = computer.getSize()+ending;
-    } else {
-        ending = "cards";
-    }
+console.log(gods[1]);
 
-    playerStack = playerCards.getSize()+1+ending;
+// var cards = {
+//     "gods": { 
+//         "hades": [100, 65, 90, 25],
+//         "cronus": [50, 20, 30, 65], 
+//         "persephone": [0, 40, 0, 20],
+//         "aphrodite": [70, 70, 0, 85],
+//         "apollo": [70, 25, 90, 60],
+//         "ares": [100, 0, 100, 50],
+//         "artemis": [25, 0, 50, 70],
+//         "zeus": [100, 90, 90, 70],
+//         "athena": [85, 50, 70, 100],
+//         "poseidon": [100, 100, 100, 70],
+//         "hera": [100, 20, 0, 75],
+//         "hermes": [20, 60, 0, 90],
+//         "hestia": [30, 70, 15, 80],
+//         "iris": [0, 0, 0, 50],
+//         "atlas": [100, 40, 0, 100],
+//         "rhea": [80, 90, 25, 65],
+//         "basilisk": [100, 0, 90, 0],
+//         "harpy": [70, 50, 90, 30],
+//         "pheonix": [70, 90, 25, 70],
+//         "siren": [100, 80, 90, 40]
+//     }
+// }
 
-    if (computerCards.getSize()===0) {
-        ending = "card.";
-    } else {
-        ending = "cards"
-    }
+console.log(cards.gods[0]);
 
-    computerStack = (computerCards.getSize()+1+ending)
-}
+//Generate a hand of 10 cards per player
 
-mainStack = theDeck.getSize();
+// function GenerateHand (){
+//     const playerOneHand = [];
+//     while (playerOneHand.length <= 10){
+//         for (let i = 0; i <= 10; i++) {
+//             let card = (Math.Floor(Math.random * gods.length));
+//             console.log(card);
+//             playerOneHand = playerOneHand.push;
+//             console.log(playerOneHand);
+//         }
+//     }
 
-function drawPlayerOneCard() {
-    document.getElementById(p1CardName);
-    document.getElementById(p1Power);
-    document.getElementById(p1Magic);
-    document.getElementById(p1Torror);
-    document.getElementById(p1Wisdom);
-}
+
+//}
+
+
+//function playerOneScore() {
+
+//}
+
 
 
