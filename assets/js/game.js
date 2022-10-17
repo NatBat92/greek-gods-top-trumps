7a46792bccf11d9509ca48512f6d5d2e5807fbf4
NatBat92 <natalie.neale1992@outlook.com>
2022-10-17 16:33:37 +0000

    add function for player 1 drawing cards and add values for the card attributes

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,36 +2,65 @@
 
 let gods = [ 
     new mythologicalCard ("hades", 100, 65, 90, 25),
-    new mythologicalCard ("cronus", 100, 65, 90, 25),
-    new mythologicalCard ("persephone", 100, 65, 90, 25),
-    new mythologicalCard ("aphrodite", 100, 65, 90, 25),
-    new mythologicalCard ("apollo", 100, 65, 90, 25),
-    new mythologicalCard ("ares", 100, 65, 90, 25),
-    new mythologicalCard ("artemis", 100, 65, 90, 25),
-    new mythologicalCard ("zeus", 100, 65, 90, 25),
-    new mythologicalCard ("athena", 100, 65, 90, 25),
-    new mythologicalCard ("poseidon", 100, 65, 90, 25),
-    new mythologicalCard ("hera", 100, 65, 90, 25),
-    new mythologicalCard ("hermes", 100, 65, 90, 25),
-    new mythologicalCard ("hestia", 100, 65, 90, 25),
-    new mythologicalCard ("iris", 100, 65, 90, 25),
-    new mythologicalCard ("atlas", 100, 65, 90, 25),
-    new mythologicalCard ("rhea", 100, 65, 90, 25),
-    new mythologicalCard ("basilisk", 100, 65, 90, 25),
-    new mythologicalCard ("harpy", 100, 65, 90, 25),
-    new mythologicalCard ("pheonix", 100, 65, 90, 25),
-    new mythologicalCard ("siren", 100, 65, 90, 25),
+    new mythologicalCard ("cronus", 50, 20, 30, 65),
+    new mythologicalCard ("persephone", 0, 40, 0, 20),
+    new mythologicalCard ("aphrodite", 70, 70, 0, 85),
+    new mythologicalCard ("apollo", 70, 25, 90, 60),
+    new mythologicalCard ("ares", 100, 0, 100, 50),
+    new mythologicalCard ("artemis", 25, 0, 50, 70),
+    new mythologicalCard ("zeus", 100, 90, 90, 70),
+    new mythologicalCard ("athena", 85, 50, 70, 100),
+    new mythologicalCard ("poseidon", 100, 100, 100, 70),
+    new mythologicalCard ("hera", 100, 20, 0, 75),
+    new mythologicalCard ("hermes", 20, 60, 0, 90),
+    new mythologicalCard ("hestia", 30, 70, 15, 80),
+    new mythologicalCard ("iris", 0, 0, 0, 50),
+    new mythologicalCard ("atlas", 100, 40, 0, 100),
+    new mythologicalCard ("rhea", 80, 90, 25, 65),
+    new mythologicalCard ("basilisk", 100, 0, 90, 0),
+    new mythologicalCard ("harpy", 70, 50, 90, 30),
+    new mythologicalCard ("pheonix", 70, 90, 25, 70),
+    new mythologicalCard ("siren", 100, 80, 90, 40),
 ];
 
-let allCards = [hades, cronus, persephone, aphrodite, apollo, ares, artemis, zeus, athena, poseidon, hera, hermes, hestia, iris, atlas, rhea, basilisk, harpy, pheonix, siren]
-
+var content = "";
+var result = "";
+var endGame = false;
+var noGo = false;
+var noValue = -1;
 let playerCards;
 let computerCards;
-let playerPoints;
-let computerPoints;
-let currentPlayerCard;
-let currentComputerCard;
+var player, computer, theDeck, playerOneCard, playerTwoCard, ending, computerStack, playerStack, mainStack, g, i, j, cardNum;
 
-function shuffleCards() {
-    let currentIndex = allCards.length;
+
+function draw() {
+    if (endGame) {
+        ending = "cards";
+        playerCards = player.getSize()+ending;
+        computerCards = computer.getSize()+ending;
+    } else {
+        ending = "cards";
+    }
+
+    playerStack = playerCards.getSize()+1+ending;
+
+    if (computerCards.getSize()===0) {
+        ending = "card.";
+    } else {
+        ending = "cards"
+    }
+
+    computerStack = (computerCards.getSize()+1+ending)
 }
+
+mainStack = theDeck.getSize();
+
+function drawPlayerOneCard() {
+    document.getElementById(p1CardName);
+    document.getElementById(p1Power);
+    document.getElementById(p1Magic);
+    document.getElementById(p1Torror);
+    document.getElementById(p1Wisdom);
+}
+
+
